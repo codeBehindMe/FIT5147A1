07c84b14a132ef792e3ebada17bf629f38ae9e96
codeBehindMe <aaron.tillekeratne@simplyenergy.com.au>
2017-03-17 16:42:01 +1100

    did some more tableau

---
+++ PyCode/Assignment1Code/xformVis.py
@@ -0,0 +1,59 @@
+# Just do some transforms for kicks.
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb
+import math
+import re
+
+_dt = pd.read_csv("C:\\Users\\tillera\\Documents\\FIT5147\\FIT5147A1\\census2011_with_crime.csv")
+
+sb.jointplot(_dt['A'], _dt['B'], kind='scatter')
+
+_dtSub = _dt[['Code', 'A', 'B']]
+
+
+_dtSubMelt = pd.melt(_dtSub,['Code'],['A','B'])
+
+_dtSubMelt.columns = ['POSTCODE','CRIMETYPE','CRIMES']
+
+
+def crimeType(x):
+    if x == 'A':
+        return 'Personal'
+    elif x == 'B':
+        return 'Property'
+    else:
+        return None
+
+
+_dtSubMelt['CRIMETYPE'] = _dtSubMelt['CRIMETYPE'].apply(lambda x: crimeType(x))
+
+_dtSubMelt.to_csv("C:\\Users\\tillera\\Documents\\FIT5147\\FIT5147A1\\crimePCodeMelt.csv",index=False)
+
+_dtAges = _dt[['Code', '0-9F', '0-9M', '10-19F', '10-19M', '20-29F', '20-29M', '30-39F', '30-39M', '40-49F', '40-49M', '50-59F', '50-59M', '60-69F', '60-69M', '70-79F', '70-79M', '80-89F', '80-89M', '90+F', '90+M','A','B'
+]]
+
+def xFormMax(dataFrame):
+
+    df = dataFrame.copy()
+
+    ss = df.ix[:,1:21]
+
+    for i,r in ss.iterrows():
+        df.set_value(i,'MAXAGEFREQ',r.idxmax(axis = 1))
+
+    return df
+
+_dtAges = xFormMax(_dtAges)
+
+_dtAges = _dtAges[['Code','A','B','MAXAGEFREQ']]
+
+_dtAges = pd.melt(_dtAges,['Code','MAXAGEFREQ'],['A','B'])
+
+_dtAges.columns = ['POSTCODE','FREQUENTAGE','CRIMETYPE','CRIMES']
+
+_dtAges['CRIMETYPE'] = _dtAges['CRIMETYPE'].apply(lambda x : crimeType(x))
+
+_dtAges.to_csv("C:\\Users\\tillera\\Documents\\FIT5147\\FIT5147A1\\crimeWithAge.csv",index=False)
